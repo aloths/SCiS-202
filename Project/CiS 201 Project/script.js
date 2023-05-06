acdb8d128734e7fcf79663332722051c33e64002
Sarah Rashed <127112041+aloths@users.noreply.github.com>
2023-05-06 03:24:51 +0300

    Add files via upload

---
+++ Project/CiS 201 Project/script.js
@@ -0,0 +1,9 @@
+
+function imgSlider(anything){
+    document.querySelector('.PRS1').src = anything;
+}
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
