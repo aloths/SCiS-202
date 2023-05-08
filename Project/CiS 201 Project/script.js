df633a8e704a2e261de348ebdd0a45e5920e6e0d
Sarah Rashed <127112041+aloths@users.noreply.github.com>
2023-05-08 17:02:50 +0300

    Delete script.js

--- Project/CiS 201 Project/script.js
+++
@@ -1,9 +0,0 @@
-
-function imgSlider(anything){
-    document.querySelector('.PRS1').src = anything;
-}
-function changeCircleColor(color){
-    const circle = document.querySelector('.circle');
-    circle.style.background = color;
-}
-
